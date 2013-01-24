12b75a1a908c930c403a53aace0631f02bb8be7e
Riley Moses <victi.resurgo@gmail.com>
2013-01-24 12:46:28 -0700

    Add methods to render form, list, and 404

--- project02/albums.rb
+++ project02/albums.rb
@@ -2,8 +2,28 @@ require 'rack'
 
 class AlbumOrganizer
 	def call(env)
-		[200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+		request = Rack::Request.new(env)
+		case request.path
+		when "/form" then render_form(request)
+		when "/list" then render_list(request)
+		else render_404(env)
+		end
+	end
+
+	def render_form(request)
+		response = Rack::Response.new
+		File.open("form.html", "rb") { |form| response.write(form.read) }
+		response.finish
+	end
+
+	def render_list(request)
+		response = Rack::Response.new(request.path)
+		response.finish
+	end
+
+	def render_404(env)
+		[404, {"Content-Type" => "text/plain"}, ["env: #{env.to_s}"]]
 	end
 end
 
-Rack::Handler::WEBrick.run(AlbumOrganizer.new, :Port => 8080)
+Rack::Handler::WEBrick.run(AlbumOrganizer.new, {:Port => 8080})
