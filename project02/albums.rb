c7b20ce767e09b02ce65840cdad51efe224dbb7e
Riley Moses <victi.resurgo@gmail.com>
2013-01-24 12:04:26 -0700

    Rename HelloWorld class to AlbumOrganizer

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,9 +1,9 @@
 require 'rack'
 
-class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
-  end
+class AlbumOrganizer
+	def call(env)
+		[200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+	end
 end
 
-Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
+Rack::Handler::WEBrick.run(AlbumOrganizer.new, :Port => 8080)
