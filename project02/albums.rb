cd217ec7da9b7d46f6ffcbf8612751fa5dda924e
Riley Moses <victi.resurgo@gmail.com>
2013-01-25 15:20:49 -0700

    Generate rank dropdown in Ruby, add comments

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,12 +1,16 @@
 require 'rack'
 
 class AlbumOrganizer
+	# Order of the table columns
 	@@COL_RANK = 0
 	@@COL_NAME = 1
 	@@COL_YEAR = 2
 
+	# Read from the albums file when the AlbumOrganizer is created
 	def initialize
+		# Save the albums in an array
 		@albums = Array.new
+		# Albums are listed in order in the text file
 		rank = 1
 		File.open("top_100_albums.txt", "r").each_line do |line|
 			@albums << [rank] + line.chomp.split(", ")
@@ -17,9 +21,11 @@ class AlbumOrganizer
 	def call(env)
 		request = Rack::Request.new(env)
 		case request.path
+		# Redirect "/" to "/form"
 		when "/" then [ 302, {'Location'=> '/form' }, [] ]
 		when "/form" then render_form(request)
 		when "/list" then render_list(request)
+		# Provde path to CSS file for "/list"
 		when "/list.css" then render_list_css
 		else render_404(env)
 		end
@@ -27,32 +33,56 @@ class AlbumOrganizer
 
 	private
 
+	# Display the form
 	def render_form(request)
 		response = Rack::Response.new
+		# Write partial HTML
 		File.open("form.html", "rb") { |form| response.write(form.read) }
+		# Generate HTML for rank dropdown
+		response.write(generate_rank_values)
+		# Add submit button and close form
+		response.write("<input type=\"submit\" value=\"Display List\"></form>")
+		# Finish HTML response
+		response.write(closing_html)
 		response.finish
 	end
 
+	# Create HTML select dropdown with values 1 through 100
+	def generate_rank_values
+		list = "<select name=\"rank\">"
+		for i in 1..100
+			list += "<option value=\"#{i}\">#{i}</option>"
+		end
+		list += "</select>"
+	end
+
+	# Display the results list (table)
 	def render_list(request)
 		response = Rack::Response.new
+		# Write partial HTML
 		File.open("list.html", "rb") { |list| response.write(list.read) }
+		# Generate "sorted by" text
 		response.write(generate_sorted_by(request))
+		# Generate the results
 		response.write(generate_sorted_table(request))
+		# Finish HTML response
 		response.write(closing_html)
-		#puts "body:", response.body
 		response.finish
 	end
 
+	# Provide list.css file requested by list.html
 	def render_list_css
 		response = Rack::Response.new
 		File.open("list.css", "rb") { |css| response.write(css.read) }
 		response.finish
 	end
 
+	# Any invalid path will display the value of env
 	def render_404(env)
 		[404, {"Content-Type" => "text/plain"}, ["env: #{env.to_s}"]]
 	end
 
+	# Sort the albums array and return HTML for the "sorted by" text
 	def generate_sorted_by(request)
 		# Sort the albums based on the "order" param from query string
 		order = request.GET()["order"]
@@ -66,8 +96,11 @@ class AlbumOrganizer
 		"<p>Sorted by #{order}</p>"
 	end
 
+	# Generate HTML for the results table based on the query
 	def generate_sorted_table(request)
+		# Get the rank to highlight from the query string
 		rank_to_highlight = request.GET()["rank"].to_i
+		# Generate table HTML
 		table_content = "<table>"
 		@albums.each do |album|
 			if album[@@COL_RANK] == rank_to_highlight
@@ -80,6 +113,7 @@ class AlbumOrganizer
 		table_content += "</table>"
 	end
 
+	# Close body and html tags
 	def closing_html
 		"</body></html>"
 	end
