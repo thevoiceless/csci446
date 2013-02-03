d8b4c8bdcd5131d636596533a635e6dc88dae639
Riley Moses <victi.resurgo@gmail.com>
2013-02-03 12:20:32 -0700

    Convert list.html to list.html.erb

--- project03/albums.rb
+++ project03/albums.rb
@@ -40,30 +40,24 @@ class AlbumOrganizer
 		response.finish
 	end
 
-	# Create HTML select dropdown with values 1 through 100
-	def generate_rank_values
-		list = "<select name=\"rank\">"
-		for i in 1..100
-			list += "<option value=\"#{i}\">#{i}</option>"
-		end
-		list += "</select>"
-	end
-
 	# Display the results list (table)
 	def render_list(request)
+		# Sort the albums based on the "order" param from query string
+		case request.GET()["order"]
+		when "rank" then @albums.sort_by! { |album| album[@@COL_RANK] }
+		when "name" then @albums.sort_by! { |album| album[@@COL_NAME] }
+		when "year" then @albums.sort_by! { |album| album[@@COL_YEAR] }
+		else @albums.sort_by! { |album| album[@@COL_RANK] }
+		end
+		# Get the rank to highlight from the query string
+		rank_to_highlight = request.GET()["rank"].to_i
+
 		response = Rack::Response.new
-		# Write partial HTML
-		File.open("list.html", "rb") { |list| response.write(list.read) }
-		# Generate "sorted by" text
-		response.write(generate_sorted_by(request))
-		# Generate the results
-		response.write(generate_sorted_table(request))
-		# Finish HTML response
-		response.write(closing_html)
+		response.write(ERB.new(File.read("list.html.erb")).result(binding))
 		response.finish
 	end
 
-	# Provide list.css file requested by list.html
+	# Provide list.css file requested by list.html.erb
 	def render_list_css
 		response = Rack::Response.new
 		File.open("list.css", "rb") { |css| response.write(css.read) }
@@ -74,44 +68,9 @@ class AlbumOrganizer
 	def render_404(env)
 		[404, {"Content-Type" => "text/plain"}, ["env: #{env.to_s}"]]
 	end
-
-	# Sort the albums array and return HTML for the "sorted by" text
-	def generate_sorted_by(request)
-		# Sort the albums based on the "order" param from query string
-		order = request.GET()["order"]
-		case order
-		when "rank" then @albums.sort_by! { |album| album[@@COL_RANK] }
-		when "name" then @albums.sort_by! { |album| album[@@COL_NAME] }
-		when "year" then @albums.sort_by! { |album| album[@@COL_YEAR] }
-		else @albums.sort_by! { |album| album[@@COL_RANK] }
-		end
-		# Return the HTML displaying the sort order
-		"<p>Sorted by #{order}</p>"
-	end
-
-	# Generate HTML for the results table based on the query
-	def generate_sorted_table(request)
-		# Get the rank to highlight from the query string
-		rank_to_highlight = request.GET()["rank"].to_i
-		# Generate table HTML
-		table_content = "<table>"
-		@albums.each do |album|
-			if album[@@COL_RANK] == rank_to_highlight
-				table_content += "<tr id=\"highlight\"><td>#{album[@@COL_RANK]}</td>"
-			else
-				table_content += "<tr><td>#{album[@@COL_RANK]}</td>"
-			end
-			table_content += "<td>#{album[@@COL_NAME]}</td><td>#{album[@@COL_YEAR]}</td></tr>"
-		end
-		table_content += "</table>"
-	end
-
-	# Close body and html tags
-	def closing_html
-		"</body></html>"
-	end
 end
 
+# Handle Control+C
 Signal.trap('INT') {
 	Rack::Handler::WEBrick.shutdown
 }
