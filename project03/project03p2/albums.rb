18b2f216458b3b0415c99824188764ddd9b3282d
Riley Moses <victi.resurgo@gmail.com>
2013-02-03 13:18:13 -0700

    Set up initial redirect to form

--- project03/project03p2/albums.rb
+++ project03/project03p2/albums.rb
@@ -2,9 +2,23 @@ require 'sinatra'
 require 'data_mapper'
 require_relative 'Album'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
-set :port, 8080
+class AlbumOrganizer < Sinatra::Base
 
-get "/" do
-  "Sinatra is working!"
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
+	set :port, 8080
+
+	get "/" do
+		redirect "/form"
+	end
+
+	get "/form" do
+		"foo"
+	end
 end
+
+# Handle Control+C
+Signal.trap('INT') {
+	Rack::Handler::WEBrick.shutdown
+}
+
+Rack::Handler::WEBrick.run(AlbumOrganizer.new, {:Port => 8080})
