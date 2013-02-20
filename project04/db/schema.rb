66eccaacd6cfc1be1e165af70a96f2c7179b4af2
Riley Moses <victi.resurgo@gmail.com>
2013-02-19 17:44:27 -0700

    Convert 2 spaces to 4-space tabs

--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(:version => 20130219195053) do
 
-  create_table "products", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 2
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-  end
+	create_table "products", :force => true do |t|
+		t.string   "title"
+		t.text     "description"
+		t.string   "image_url"
+		t.decimal  "price",       :precision => 8, :scale => 2
+		t.datetime "created_at",                                :null => false
+		t.datetime "updated_at",                                :null => false
+	end
 
 end
