15bd5ae5a28e68fb3c01f3c3801d70ef8583f26c
Riley Moses <victi.resurgo@gmail.com>
2013-02-20 17:58:59 -0700

    Add fixture data, unit test for unique title

--- project04/test/unit/product_test.rb
+++ project04/test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
+
 	def new_product(image_url)
 		Product.new(title: "My Book Title", 
 			description: "yyy", 
@@ -44,5 +46,14 @@ class ProductTest < ActiveSupport::TestCase
 		bad.each do |name|
 			assert(new_product(name).invalid?, "#{name} shouldn't be valid")
 		end
+	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(title: products(:ruby).title,
+			description: "yyy",
+			price: 1,
+			image_url: "fred.gif")
+		assert !product.save
+		assert_equal("has already been taken", product.errors[:title].join('; '))
 	end
 end
