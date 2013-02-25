d0435123667c5d459525ec9569442dee624aac6c
Riley Moses <victi.resurgo@gmail.com>
2013-02-25 14:19:33 -0700

    Add tests for new store functionality (finish ch. 8)

--- project04/test/functional/store_controller_test.rb
+++ project04/test/functional/store_controller_test.rb
@@ -4,6 +4,14 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    # Looks for an element named 'a' contained in an element with an id of
+    # 'side' contained within an element with an id of 'columns', verifies
+    # that there are a minimum of 4 such elements
+    assert_select('#columns #side a', minimum: 4)
+    # Verify that all products are displayed
+    assert_select('#main .entry', 3)
+    assert_select('h3', 'Programming Ruby 1.9')
+    assert_select('.price', /\$[,\d]+\.\d\d/)
   end
 
 end
