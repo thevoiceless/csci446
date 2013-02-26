6e4d4dd960418958b037e66ad893a02e2f80fd72
Riley Moses <victi.resurgo@gmail.com>
2013-02-26 12:46:08 -0700

    Add has_many relationship to product, validation before destroy

--- project04/app/models/product.rb
+++ project04/app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ActiveRecord::Base
 	attr_accessible :description, :image_url, :price, :title
 
+	has_many :line_items
+
+	# If the hook method returns false, the row will not be destroyed
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	# Compare to 0.01 instead of zero because entering a price of 0.001 would
@@ -10,4 +15,15 @@ class Product < ActiveRecord::Base
 		with:    %r{\.(gif|jpg|png)$}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.emtpy?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
 end
