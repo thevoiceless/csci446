5a4dc79490cf5916ee4f5d7343616e8ea4bb2061
Riley Moses <victi.resurgo@gmail.com>
2013-02-26 12:41:42 -0700

    Add belongs_to relationships to line_item

--- project04/app/models/line_item.rb
+++ project04/app/models/line_item.rb
@@ -1,3 +1,7 @@
 class LineItem < ActiveRecord::Base
 	attr_accessible :cart_id, :product_id
+
+	# No line item can exist without the corresponding cart and product rows
+	belongs_to :product
+	belongs_to :cart
 end
