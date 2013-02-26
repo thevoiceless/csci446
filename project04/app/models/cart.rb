a9205c43fefe975f6c139e1947cbeb90456a762f
Riley Moses <victi.resurgo@gmail.com>
2013-02-26 12:38:25 -0700

    Spaces to tabs, add has_many relationship to cart

--- project04/app/models/cart.rb
+++ project04/app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ActiveRecord::Base
-  # attr_accessible :title, :body
+	# attr_accessible :title, :body
+
+	# A cart has many line items
+	# The existence of line items is dependent on the existence of the cart
+	has_many :line_items, dependent: :destroy
 end
--- project04/app/models/line_item.rb
+++ project04/app/models/line_item.rb
@@ -1,3 +1,3 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+	attr_accessible :cart_id, :product_id
 end
