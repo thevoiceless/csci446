5cdedc783a6c0a1c69924bd3145e6763fea2338f
Riley Moses <victi.resurgo@gmail.com>
2013-02-26 12:18:27 -0700

    Add private current_cart method to ApplicationController

--- project04/app/controllers/application_controller.rb
+++ project04/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+
+	private
+
+		# Placing this method in ApplicationController and marking it private
+		# makes the method available only to controllers and prevents Rails
+		# from making it available as an action on the controller
+		def current_cart
+			Cart.find(session[:cart_id])
+		rescue ActiveRecord::RecordNotFound
+			cart = Cart.create
+			session[:cart_id] = cart.id
+			cart
+		end
 end
