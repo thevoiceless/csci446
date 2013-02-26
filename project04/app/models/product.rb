6897494c87aaede223284ff0c04aa4b4aa54618c
Riley Moses <victi.resurgo@gmail.com>
2013-02-26 13:18:08 -0700

    Modify line_item create() method to add products, update tests

--- project04/app/models/product.rb
+++ project04/app/models/product.rb
@@ -19,7 +19,7 @@ class Product < ActiveRecord::Base
 	private
 
 	def ensure_not_referenced_by_any_line_item
-		if line_items.emtpy?
+		if line_items.empty?
 			return true
 		else
 			errors.add(:base, 'Line Items present')
