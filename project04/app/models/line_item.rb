040876795a32ff3c238713eada0a9ae39c3c0c14
Riley Moses <victi.resurgo@gmail.com>
2013-02-28 10:49:01 -0700

    Add combine_items_in_cart migration

--- project04/app/models/line_item.rb
+++ project04/app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-	attr_accessible :cart_id, :product_id
+	attr_accessible :cart_id, :product_id, :quantity
 
 	# No line item can exist without the corresponding cart and product rows
 	belongs_to :product
--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130228165758) do
+ActiveRecord::Schema.define(:version => 20130228172019) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
